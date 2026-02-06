36b973de74a547fe868858b71f1e847c10367f28
sonhoang002 <120953254+sonhoang002@users.noreply.github.com>
2026-02-06 12:44:39 -0500

    newSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./css/topHalf.css";
 import "./css/main.css";
 import "./css/footer.css";
+import flow from "./flow.png";
+
+const ss = document.querySelector('.screenshot[number="1"]');
+
+ss.style.backgroundImage = `url(${flow})`;
+ss.style.backgroundSize = "cover"; // optional: make it cover the div
+ss.style.backgroundPosition = "center"; // optional: center the image
